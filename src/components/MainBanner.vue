ae267f451e44f94a4a4519c898991bacdab4072b
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2019-09-30 22:19:45 +0200

    fixed compile style issue

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="banner">
     <img class="banner__img" src="../assets/img/slider.jpg" alt="Fashion Clothing Brand" />
-    <div class="banner__container">
+    <div class="container banner__wrapper">
       <a class="btn btn--banner" href="#">Shop men's collection</a>
     </div>
   </section>
